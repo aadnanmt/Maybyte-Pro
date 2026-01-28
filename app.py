e27e9b9a3afda6cd110b168eaf83c84772ce18e2
aadnanmt <aadnanmtcontact@gmail.com>
2026-01-28 12:54:03 +0700

    feat: recommended for setting development or production

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-
 from flask import Flask, render_template, jsonify
 import monitor  
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
-
 @app.route('/api/stats')
-def stats():
-    
-    
+def stats():    
     data = monitor.ambil_data_cpu() 
-    
-    
     return jsonify(data)
 
+# recommended from m to production
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run
+
+# enable to development
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000) 
