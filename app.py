4874387b246ba96d65de9bddfc5f7feff08f5460
aadnanmt <aadnanmtcontact@gmail.com>
2026-01-28 13:04:32 +0700

    feat: import Flask Minify

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
+from flask_minify import Minify
 import monitor  
 
 app = Flask(__name__)
+Minify(app=app, html=True, js=True, cssless=True)
 
 @app.route('/')
 def index():
