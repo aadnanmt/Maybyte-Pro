b767eca27ee971e5e7f21669994c81531045c90c
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-01-28 12:56:46 +0700

    chore: change multiple views and tags

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,23 @@
-        async function updateDashboard() {
-            try {
-                // request data server (be-python)
-                let response = await fetch('/api/stats');
-                
-                // change data server to  object js
-                let data = await response.json(); 
-                
-                // change interface use DOM
-                document.getElementById('cpu-val').innerText = data.cpu;
-                document.getElementById('core-val').innerText = data.core;
-                document.getElementById('thread-val').innerText = data.cpu_threads;
-                document.getElementById('status-msg').innerText = data.status;
+async function updateDashboard() {
+    try {
+        // request data server (be-python)
+        let response = await fetch('/api/stats');
 
-            } catch (error) {
-                console.log("Waduh error cuy: ", error);
-            }
-        }
+        // change data server to  object js
+        let data = await response.json();
 
-        // running updateDashboard every 2 seconds, u can change
-        setInterval(updateDashboard, 2000);
-        updateDashboard();
+        // change interface use DOM
+        document.getElementById('cpu-val').innerText = data.cpu;
+        document.getElementById('core-val').innerText = data.core;
+        document.getElementById('thread-val').innerText = data.cpu_threads;
+        document.getElementById('status-msg').innerText = data.status;
+        document.getElementById('mode').innerText = data.mode;
+
+    } catch (error) {
+        console.log("Waduh error cuy: ", error);
+    }
+}
+
+// running updateDashboard every 2 seconds, u can change
+setInterval(updateDashboard, 2000);
+updateDashboard();
