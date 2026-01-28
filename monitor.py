ef92785ae1ca225984d41ecfd33311b439860eac
aadnanmt <aadnanmtcontact@gmail.com>
2026-01-28 12:58:27 +0700

    feat: feature data dummy for maybyte on PaaS

--- monitor.py
+++ monitor.py
@@ -1,24 +1,50 @@
-import psutil
+# import psutil
+import random
+
+try:
+    import psutil
+
+    MODE_ASLI = True
+
+except ImportError:
+
+    MODE_ASLI = False 
 
 def ambil_data_cpu():
-	persentase_cpu = psutil.cpu_percent(interval=None)
-	cpu_core = psutil.cpu_count(logical=False)
-	cpu_threads = psutil.cpu_count(logical=True)
+    data = {}
 
-	mentahan_data = {
-		'cpu': persentase_cpu,
-		'status': 'Masih aman cuy' if persentase_cpu < 85 else 'cpu Throttling cuy',
-		'core': cpu_core,
-		'cpu_threads': cpu_threads
-	}
+    if MODE_ASLI:
+        cpu_percent = psutil.cpu_percent(interval=None)
+        core = psutil.cpu_count(logical=False)
+        threads = psutil.cpu_count(logical=True)
 
-	return mentahan_data
+        data = {
+            "cpu": cpu_percent,
+            "core": core,
+            "cpu_threads": threads,
+            "status": "Running on Hardware",
+            "mode": "DATA REAL"
+        }
+    
+    else:
+        fake_cpu = random.randint(5, 96)
+
+        data = {
+            "cpu": fake_cpu,
+            "core": 6,
+            "cpu_threads": 12,
+            "status": "Running on Data Dummy",
+            "mode": "DATA FAKE {DUMMY}"
+        }
+
+    if data["cpu"] < 50:
+        data["status_msg"] = "Aman terkendali cuyy"
+    elif data["cpu"] < 80:
+        data["status_msg"] = "Mulai overload"
+    else:
+        data["status_msg"] = "OVERLOAD CUYYY!"
+
+    return data
 
 if __name__ == '__main__':
-	print('Sdnag memantau cpu')
-	print("\n")
-	
-	hasil_data_mentahan = ambil_data_cpu()
-	print('laporan dari psutil soal persentase cpu', hasil_data_mentahan)
-	print("\n")
-	print("laporan dari psutil soal core dan threads", hasil_data_mentahan)
+    print(ambil_data_cpu())
