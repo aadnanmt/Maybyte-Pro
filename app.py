d8fbb312fff6a14f91d4fc01be99ba7dc5d85f54
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-04-06 15:55:22 +0700

    feat: module security header, validate request same origin, chage on function stats

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from flask_minify import Minify
-import monitor  
+from backend.monitor import ambil_data_cpu
+from backend.security import validate_ajax_request
 
 app = Flask(__name__)
 Minify(app=app, html=True, js=True, cssless=True)
@@ -10,14 +11,15 @@ def index():
     return render_template('index.html')
 
 @app.route('/api/stats')
-def stats():    
-    data = monitor.ambil_data_cpu() 
+@validate_ajax_request
+def stats():
+    data = ambil_data_cpu() 
     return jsonify(data)
 
 # recommended from m to production
 if __name__ == '__main__':
-    app.run
+    app.run()
 
-# enable to development
+# enable to development mode
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5000) 
